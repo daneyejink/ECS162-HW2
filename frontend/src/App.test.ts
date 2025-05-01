dd6b1df1323938288c149a627e884ee2cf08cfd1
Diane Kim <dianekim002@gmail.com>
2025-05-01 12:58:56 -0700

    Add initial test for App title

--- frontend/src/App.test.ts
+++ frontend/src/App.test.ts
@@ -2,6 +2,8 @@ import { test } from 'vitest';
 import { render } from '@testing-library/svelte';
 import App from './App.svelte';
 
-test('App', async () => {
-    render(App);
+test('App displays correct title', async () => {
+    const { getByText } = render(App);
+    const title = getByText('The New York Times');
+    expect(title).toBeInTheDocument();
 });
